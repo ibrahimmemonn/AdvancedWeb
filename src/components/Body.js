48f17813dbc1e01af5ccea6559c1c9d60605cef3
Ibrahim Memon <ibrahim@webhr.email>
2022-11-28 13:05:56 +0500

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "./home/Banner";
 import Header from "./home/Header";
 
 export const Body = () => {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 };
